880069b2ca07bd049f89414c43a7e8db86834204
SieglStefan <stefan.siegl02@gmail.com>
2026-07-29 17:58:31 +0200

    Stop tracking results_OLD

--- results_OLD/setup.jl
+++
@@ -1,8 +0,0 @@
-### Convenience script for loading NeuralParam for experiments
-
-using Pkg
-Pkg.activate(joinpath(@__DIR__, ".."))          
-
-include(joinpath(@__DIR__, "..", "src", "NeuralParam.jl"))
-
-using .NeuralParam                                        
